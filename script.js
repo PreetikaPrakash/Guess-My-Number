1ceec26ea81a8e6bcf4e5e5f100e256eb9d9892e
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-02-18 23:28:49 +0530

    Made changes to script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
-var s = 10;
-console.log(s);
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Guess';
+
+document.querySelector('.guess').value = 34;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const your_number = Number(document.querySelector('.guess').value);
+  console.log(your_number, typeof your_number);
+
+  if (!your_number) {
+    console.log('ee');
+  }
+});
